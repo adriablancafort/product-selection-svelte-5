a73e3e23c8f25256b2dd15a194d024d7f3c97a51
Adria Blancafort <adriablancafort@gmail.com>
2024-06-24 01:55:40 +0200

    choose image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,13 +2,31 @@
     import product from './product.json';
 
     let selectedVariant = $state(product.variants[0]);
+    let selectedImage = $state(product.variants[0].image || 0);
+
+    function selectVariant(attributeSlug, option) {
+        selectedVariant = product.variants.find(variant => variant.options[attributeSlug] === option);
+        selectedImage = selectedVariant.image || selectedImage;
+    }
 </script>
-    
+
 <p>Discount: <span class="text-white bg-red-500 px-2">-{selectedVariant.discount || product.discount}%</span></p>
-<img src="{selectedVariant.image || product.image}" alt="{selectedVariant.title || product.title}">
+<img src="{product.images[selectedImage]}" alt="{selectedVariant.title || product.title}">
+
+<div class="flex space-x-2 py-2">
+    {#each product.thumbnails as thumbnail, index}
+        <img 
+            src="{thumbnail}"
+            alt="{selectedVariant.title || product.title}"
+            class="w-16 h-16 cursor-pointer {index === selectedImage ? 'ring' : ''}"
+            onmouseenter={() => selectedImage = index}
+        >
+    {/each}
+</div>
+
 <h1 class="text-4xl">{selectedVariant.title || product.title}</h1>
 <p>Price: ${selectedVariant.price || product.price}</p>
-<p>Stock: {selectedVariant.stock || product.stock}</p>
+<p>{selectedVariant.stock || product.stock ? 'In Stock' : 'Out of Stock'}</p>
 
 {#each product.attributes as attribute}
     <p>Select {attribute.title}:</p>
@@ -16,7 +34,7 @@
         {#each attribute.options as option}
             <button
                 class="px-4 {option === selectedVariant.options[attribute.slug] ? "ring" : ""}"
-                onclick={() => {selectedVariant = product.variants.find(variant => variant.options[attribute.slug] === option)}}
+                onclick={() => selectVariant(attribute.slug, option)}
             >
                 {option}
             </button>
